ed13584380d3d89e92af70cc8e3cb583c4b95bfb
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-09-22 19:06:03 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -19,8 +19,13 @@ async def group62(client, message):
         return
     if message.text.startswith("https://drive.google.com/file") and " " not in message.text.strip():
         id = message.text.replace("https://drive.google.com/file/d/","").split("/")[0]
-        await message.reply_text("Tumepokea link yako tunaifanyia kaz sio mda mrefu")
-        return
+        sts=await add_link(id+str(message.from_user.id),nyva)
+        if sts== True:
+            await message.reply_text("Tumepokea link yako tunaifanyia kaz sio mda mrefu")
+            return
+        else:
+            await message.reply_text("Samahani mpendwa mwisho tunapokea link tano ngoja tumalzie kisha ndio utatuma nyingine tano")
+            return
     else:
         await message.reply_text("boss umekosea hakiki tena link hii hakisha haina nafasi katikati na hujaongeza neno lolote mbele")
         return

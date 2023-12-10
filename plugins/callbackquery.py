afd626853a1a3adba86b3dbf17f88f4cd8ad5d55
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-12-10 22:56:12 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -57,7 +57,7 @@ async def group62(client, message):
     cnt=0
     jkz=[]
     while azb=="start":  
-        path="/gdrive/"
+        path="/drive/"
         az="dfg"
         filez=await get_gdrive_link(nyva)
         for link in filez:

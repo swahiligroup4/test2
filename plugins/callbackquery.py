240341569df7e483faa5dd74314d0763ced126b6
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2024-01-10 12:26:46 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -87,7 +87,7 @@ async def group62(client, message):
         if 'signin' in response.url:
             await User.collection.delete_one({'_id':id+"##"+str(user_id)})
             asyncio.sleep(120)
-            await mkv22.reply_text("Tafadhal kabadilishe hii link iwe iwe shared to everyone kisha itume tena kama huelew muulize @hrm45")
+            await mkv22.reply_text("🚫TAFADHALI kabadilishe hii link iwe iwe shared to everyone kisha itume tena kama huelew muulize @hrm45")
             continue
         try:
             header = response.headers['Content-Disposition']
